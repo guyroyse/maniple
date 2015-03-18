c4e58f29a3f5e696ac1e3785657836ee00758d08
Guy Royse <guy@guyroyse.com>
2015-03-18 18:20:24 -0400

    passed a few tests for select box

--- spec/maniple-spec.js
+++ spec/maniple-spec.js
@@ -3,18 +3,56 @@ describe("Maniple.TextBox", function() {
   var subject;
 
   beforeEach(function() {
-    setFixtures('<input id="textBox" />')
-    subject = Maniple.TextBox.create('#textBox');
+    setFixtures('<input id="theId" />')
+    subject = Maniple.TextBox.create('#theId');
   });
 
   it("returns the value of the text box", function() {
-    $('#textBox').val('foo');
+    $('#theId').val('foo');
     expect(subject.text()).toBe('foo');
   });
 
   it("changes the value of the text box", function() {
     subject.text('foo');
-    expect($('#textBox').val()).toBe('foo');
+    expect($('#theId')).toHaveValue('foo');
+  });
+
+});
+
+describe("Maniple.SelectBox", function() {
+
+  var subject;
+
+  beforeEach(function() {
+    setFixtures(
+      '<select id="theId">' +
+        '<option value="qux">Qux</option>' +
+      '</select>'
+    );
+
+    subject = Maniple.SelectBox.create('#theId');
+  });
+
+  when("populating with data from an array of objects with default property names of 'id' and 'name'", function() {
+
+    beforeEach(function() {
+      subject.populate([
+        { id: 'foo', name: 'Foo' },
+        { id: 'bar', name: 'Bar' },
+        { id: 'baz', name: 'Baz' }
+      ]);
+    });
+
+    it("populates the select with data from an array of objects with default property names of 'id' and 'name'", function() {
+      expect($('#theId')).toContainHtml('<option value="foo">Foo</option>')
+      expect($('#theId')).toContainHtml('<option value="bar">Bar</option>')
+      expect($('#theId')).toContainHtml('<option value="baz">Baz</option>')
+    });
+
+    it("removes previous entries", function() {
+      expect($('#theId')).not.toContainHtml('<option value="qux">Qux</option>')
+    });
+
   });
 
 });
--- src/maniple.js
+++ src/maniple.js
@@ -16,3 +16,22 @@ Maniple.TextBox = {
 
   }
 };
+
+Maniple.SelectBox = {
+  create: function(selector) {
+
+    var $selectBox = $(selector);
+
+    function populate(data) {
+      $selectBox.find('option').remove();
+      _.each(data, function(item) {
+        $selectBox.append('<option value="' + item.id + '">' + item.name + '</option>');
+      });
+    }
+
+    return {
+      populate: populate
+    };
+
+  }
+};
